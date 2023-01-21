634e658c0932b9ae6457453aa0677547ed315ba6
Glenio Juste <glenio.juste@nexus.eti.br>
2023-01-20 22:18:34 -0300

    Componente editar

--- src/app/componentes/pensamentos/pensamento.service.ts
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -20,6 +20,11 @@ export class PensamentoService {
     return this.http.post<Pensamento>(this.API, pensamento_a_cadastrar)
   }
 
+  editar(pensamento_a_editar: Pensamento): Observable<Pensamento>{
+    const url = `${this.API}/${pensamento_a_editar.id}`
+    return this.http.put<Pensamento>(url, pensamento_a_editar)
+  }
+
   excluir(id: number):Observable<Pensamento>{
     const url = `${this.API}/${id}`
     return this.http.delete<Pensamento>(url)
