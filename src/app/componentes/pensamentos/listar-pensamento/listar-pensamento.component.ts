5e4d3fe9b7e456aefda2774c2f9dc6e34af38278
Glenio Juste <glenio.juste@nexus.eti.br>
2023-01-26 20:16:41 -0300

    Campo de busca

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -12,23 +12,34 @@ export class ListarPensamentoComponent implements OnInit {
   listaPensamentos: Pensamento[] = [];
   paginaAtual: number = 1;
   haMaisPensamentoAqui: boolean = true;
+  filtro: string = '';
 
   constructor(private service: PensamentoService) { }
 
   ngOnInit(): void {
-    this.service.listar(this.paginaAtual).subscribe((listaDosPensamentos) =>{
+    this.service.listar(this.paginaAtual, this.filtro).subscribe((listaDosPensamentos) =>{
       this.listaPensamentos = listaDosPensamentos;
     })
   }
 
   carregarMaisPensamentos(){
-    this.service.listar(++this.paginaAtual)
+    this.service.listar(++this.paginaAtual, this.filtro)
     .subscribe(listaPensamentosAqui =>{
         this.listaPensamentos.push(...listaPensamentosAqui);
         if(!listaPensamentosAqui.length){
           this.haMaisPensamentoAqui = false;
         }
     })
+  }
+
+  pesquisarPensamentos(){
+    this.haMaisPensamentoAqui = true;
+    this.paginaAtual = 1;
+    this.service.listar(this.paginaAtual, this.filtro)
+        .subscribe(listaPensamentoPesquisa => {
+          this.listaPensamentos = listaPensamentoPesquisa
+        })
+
   }
 
 }
