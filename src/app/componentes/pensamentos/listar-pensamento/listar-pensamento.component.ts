710dffb4548557a4f035c3bec1da37111216bf1d
Glenio Juste <glenio.juste@nexus.eti.br>
2023-01-31 20:09:11 -0300

    Finalizando a aplicação

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { PensamentoService } from './../pensamento.service';
 import { Pensamento } from './../pensamento';
 import { Component, OnInit } from '@angular/core';
@@ -14,8 +15,13 @@ export class ListarPensamentoComponent implements OnInit {
   haMaisPensamentoAqui: boolean = true;
   filtro: string = '';
   favoritos: boolean = false
+  listaFavoritos: Pensamento[] = [];
+  titulo: string = 'Meu Mural';
 
-  constructor(private service: PensamentoService) { }
+  constructor(
+    private service: PensamentoService,
+    private router: Router
+    ) { }
 
   ngOnInit(): void {
     this.service.listar(this.paginaAtual, this.filtro, this.favoritos).subscribe((listaDosPensamentos) =>{
@@ -43,13 +49,26 @@ export class ListarPensamentoComponent implements OnInit {
 
   }
 
+  carregarComponente(){
+
+    this.favoritos = false;
+    this.paginaAtual = 1;
+
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false
+    this.router.onSameUrlNavigation = 'reload'
+    this.router.navigate([this.router.url])
+
+  }
+
   listarFavoritos(){
+    this.titulo = 'Meus Favoritos';
     this.favoritos = true
     this.haMaisPensamentoAqui = true
     this.paginaAtual = 1
     this.service.listar(this.paginaAtual, this.filtro, this.favoritos)
       .subscribe(listaPensamentosFavoritos => {
         this.listaPensamentos = listaPensamentosFavoritos
+        this.listaFavoritos = listaPensamentosFavoritos
       })
   }
 
